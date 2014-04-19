4875a2b2120e0ba02dec8c0b8decb6cec54582c9
Yury Snegirev <yusnegirev@at-consulting.ru>
2014-04-19 16:30:33 +0400

    Add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,9 @@ end
 require 'bundler/setup'
 Bundler.require
 
+require 'coveralls'
+Coveralls.wear!
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
