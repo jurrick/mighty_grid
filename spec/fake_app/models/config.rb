59b937a29802751e9cae712af3c8b54d1fe6183c
Yury Snegirev <yusnegirev@at-consulting.ru>
2014-05-17 19:31:30 +0400

    Fix database config path for tests

--- spec/fake_app/models/config.rb
+++ spec/fake_app/models/config.rb
@@ -1,4 +1,4 @@
-configs = YAML.load_file('config/database.yml')
+configs = YAML.load_file('spec/fake_app/config/database.yml')
 ActiveRecord::Base.configurations = configs
 
 db_name = ENV['DB'] || 'sqlite'
