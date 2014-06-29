99c8f6aae2727d687974b0c7bd83110951a1234c
Yury Snegirev <yusnegirev@at-consulting.ru>
2014-06-29 23:53:28 +0400

    Remove trailing whitespace

--- lib/mighty_grid/column.rb
+++ lib/mighty_grid/column.rb
@@ -20,7 +20,7 @@ module MightyGrid
 
       @model = @options[:model]
       raise MightyGridArgumentError.new("Model of field for filtering should have type ActiveRecord") if @model && @model.superclass != ActiveRecord::Base
-      
+
       @attrs = @options[:html] if @options.has_key?(:html)
       @th_attrs = @options[:th_html] if @options.has_key?(:th_html)
       @title = @options.has_key?(:title) && @options[:title] || ''
