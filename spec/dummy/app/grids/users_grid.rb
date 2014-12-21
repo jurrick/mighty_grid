1fc77b3e71322b7817a8ad4ac70a6f5ca507de70
Yury Snegirev <yusnegirev@at-consulting.ru>
2014-12-21 18:04:39 +0300

    Fix filtering tests

--- spec/dummy/app/grids/users_grid.rb
+++ spec/dummy/app/grids/users_grid.rb
@@ -1,3 +1,7 @@
 class UsersGrid < MightyGrid::Base
   scope { User }
+
+  filter :name
+  filter :role, :enum, collection: [:user, :publisher, :moderator, :admin].map { |r| [r.capitalize, r] }
+  filter :is_banned, :boolean
 end
